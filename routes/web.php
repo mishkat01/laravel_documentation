4800e8aeef9753aee37839a3d280dfd4d66401c6
mishkat01 <124544908+mishkat01@users.noreply.github.com>
2024-01-16 17:14:49 +0600

    model binding

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Employees;
+use App\Models\Employee;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,11 +24,40 @@ Route::get('/greeting', function () {
 Route::get('/',[App\Http\Controllers\Employees::class,'view']);
 Route::get('/insert',[App\Http\Controllers\Employees::class,'insert'])->name('insert');
 Route::post('store_data',[App\Http\Controllers\Employees::class,'store_data']);
-// Route with Parameters
-// You may define as many route parameters as required by your route:
-Route::get('delete/{id}',[App\Http\Controllers\Employees::class,'delete']);
-Route::get('edit/{id}',[App\Http\Controllers\Employees::class,'edit']);
-Route::get('update/{id}',[App\Http\Controllers\Employees::class,'update']);
+// Grouped Routes
+Route::middleware(['api'])->group(function () {
+   // Routes that require authentication with 'auth'
+
+   // Route with Parameters
+   // You may define as many route parameters as required by your route:
+   Route::get('delete/{id}', [Employees::class, 'delete']);
+   Route::get('edit/{id}', [Employees::class, 'edit']);
+   Route::post('update/{id}', [Employees::class, 'update']);
+});
+
+
+
+ // common controller  group route 
+
+// Route::controller(Employees::class)->group(function () {
+//     Route::get('/insert', 'insert');
+//     Route::post('/store_data', 'store_data');
+// });
+
+
+//  prefix in route
+// Route::prefix('admin')->group(function () {
+//    Route::get('insert', [Employees::class, 'insert']);
+//    //http://127.0.0.1:8000/admin/insert
+//    Route::get('/', [Employees::class, 'view']);
+// });
+
+
+// Route Model Binding
+// Route::get('/{id}', function (Employee $user) {
+//    return $user;
+// });
+
 
 
 
@@ -38,12 +69,15 @@ Route::get('update/{id}',[App\Http\Controllers\Employees::class,'update']);
 // Route::patch($uri, $callback);
 // Route::delete($uri, $callback);
 // Route::options($uri, $callback);
+
 // Route::match(['get', 'post'], '/', function () {
    // ...
 // });
+
 // Route::any('/', function () {
    // ...
 // });
+
 // Route::redirect('/here', '/there');
 // Route::view('/welcome', 'welcome');
 // Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
